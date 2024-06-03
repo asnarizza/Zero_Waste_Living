c24d33f4a6047448af81404ee6a97a60ab6f350a
Rose Asnarizza <asnarizza@gmail.com>
2024-06-03 18:53:30 +0800

    register

---
+++ public/js/registerMember.js
@@ -0,0 +1,86 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Initialize the application
+    init();
+
+    // Function to initialize the application
+    function init() {
+        setupEventListeners();
+    }
+
+    // Function to set up event listeners
+    function setupEventListeners() {
+        document.getElementById('registerMember').addEventListener('submit', registerMember);
+    }
+
+    // Function to handle cafe owner registration
+    async function registerMember(event) {
+        event.preventDefault(); // Prevent the default form submission
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const gender = document.getElementById('gender').value;
+        const birthDate = document.getElementById('birthDate').value;
+        const image = document.getElementById('image').value;
+
+        const registrationData = {
+            action: "registerMember",
+            name: name,
+            email: email,
+            password: password,
+            gender: gender,
+            birthDate: birthDate,
+            image: image
+        };
+
+        console.log(registrationData)
+
+        try {
+            const response = await fetch("http://localhost/Zero_Waste_Living/api/auth/register.php?action=registerMember", {
+                // Change this URL to your actual endpoint
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(registrationData)
+            });
+
+            const result = await response.json();
+
+            if (response.ok) {
+                
+                alert('Registration successful!');
+                window.location.href = "login.html";
+                document.getElementById('studentRegistrationForm').reset();
+                console.log(result);
+            } else {
+                alert('Failed To Register Student!');
+                document.getElementById('studentRegistrationForm').reset();
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            document.getElementById('message').innerText = 'An error occurred while registering.';
+        }
+    }
+});
+
+async function setUserName() {
+
+    var userData = JSON.parse(sessionStorage.getItem("userData"));
+    var name = userData.name
+    var cafeId = userData.cafe_id;
+
+    document.querySelector('.user-info span').textContent = name;
+
+        // Process the returned data
+        if (data.totalDailyDonations && data.totalDailyDonations.totalQuantity !== undefined) {
+            const totalQuantity = data.totalDailyDonations.totalQuantity;
+            // Update the DOM element with the fetched data
+            document.querySelector('.dailyDonation .text p').textContent = totalQuantity;
+        } else {
+            document.querySelector('.dailyDonation .text p').textContent = '0';
+        }
+   
+}
+
+setUserName()
