f3313faebded65e517b77d7cb8f5489d96b6bd07
Rose Asnarizza <asnarizza@gmail.com>
2024-06-03 20:38:41 +0800

    register php

--- api/auth/register.php
+++ api/auth/register.php
@@ -7,6 +7,23 @@ $password = "";
 
 $db = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
 
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
 // initial response code
 // response code will be changed if the request goes into any of the processes
 http_response_code(404);
